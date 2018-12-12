209c69a675a75c7eb3fcd868737abc108307a9af
Gróf Attila <grof.attila9@gmail.com>
2018-12-12 23:41:14 +0100

    Model and Communication test packages and files added.

---
+++ djitello/src/test/java/hu/atig/dji/tello/communication/TelloCommunicationTest.java
@@ -0,0 +1,15 @@
+package hu.atig.dji.tello.communication;
+
+import org.junit.Test;
+
+public class TelloCommunicationTest {
+
+  private TelloCommunication telloCommunication = new TelloCommunicationImpl();
+
+  @Test
+  public void testConnect() {
+    // TODO
+  }
+
+
+}
